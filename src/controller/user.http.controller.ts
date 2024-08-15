f4aa4b2ee33adace2c13dcfeaeaa0bff61c5e161
Yasith Ishara Perera <yasithishara@gmail.com>
2024-08-15 11:43:34 +0530

    handling null values in validation

--- src/controller/user.http.controller.ts
+++ src/controller/user.http.controller.ts
@@ -21,57 +21,62 @@ async function createNewUserAccount(req: Request,
     // Data Validation
     let violations = [];
     let validated: boolean = true;
-    if (!UserValidation.validateEmail(user.email)) {
-        validated = false;
-        violations.push('Invalid email');
-    }
-    if (!UserValidation.validateName(user.name)) {
-        validated = false;
-        violations.push('Invalid name');
-    }
-    if (!UserValidation.validateContact(user.contact)) {
-        validated = false;
-        violations.push('Invalid contact');
-    }
-
-    if (!validated) {
-        res.status(400).json({error: violations.toString()});
-        console.log(violations.toString());
+    if (!user) {
+        res.sendStatus(400);
     } else {
-        const connection = await pool.connect();
-        let violations = [];
-        let validated: boolean = true;
-        // Business Validation
-        // 1. Find whether this email already exists
-        const emailResult = await connection.query('SELECT email FROM "user" WHERE email=$1'
-            , [user.email]);
-        if (emailResult.rowCount ?? 0 >= 1) {
+        if (!user.email || !UserValidation.validateEmail(user.email)) {
             validated = false;
-            violations.push('email already exists!');
+            violations.push('Invalid email');
         }
-        // 2. Find whether this contact number is already associated with another user
-        const contactResult = await connection.query('SELECT email FROM "user" WHERE contact=$1'
-            , [user.contact]);
-        if (contactResult.rowCount ?? 0 >= 1) {
+        if (!user.name || !UserValidation.validateName(user.name)) {
             validated = false;
-            violations.push('contact already associated with another user!');
+            violations.push('Invalid name');
         }
+        if (!user.contact || !UserValidation.validateContact(user.contact)) {
+            validated = false;
+            violations.push('Invalid contact');
+        }
+
         if (!validated) {
-            // If failed 409 status code ( conflict )
-            res.status(409).json({error: violations.toString()});
+            res.status(400).json({error: violations.toString()});
             console.log(violations.toString());
         } else {
-            try {
-                await connection.query('INSERT INTO "user" (email,name,contact) VALUES ($1,$2,$3)',
-                    [user.email, user.name, user.contact]);
-
-                res.sendStatus(201);
-            } catch (e) {
-                console.log(e);
-                res.status(500).json({error: e.toString()}); /* Internal server error - can't save */
-            } finally {
-                connection.release();
+            const connection = await pool.connect();
+            let violations = [];
+            let validated: boolean = true;
+            // Business Validation
+            // 1. Find whether this email already exists
+            const emailResult = await connection.query('SELECT email FROM "user" WHERE email=$1'
+                , [user.email]);
+            if (emailResult.rowCount ?? 0 >= 1) {
+                validated = false;
+                violations.push('email already exists!');
             }
+            // 2. Find whether this contact number is already associated with another user
+            const contactResult = await connection.query('SELECT email FROM "user" WHERE contact=$1'
+                , [user.contact]);
+            if (contactResult.rowCount ?? 0 >= 1) {
+                validated = false;
+                violations.push('contact already associated with another user!');
+            }
+            if (!validated) {
+                // If failed 409 status code ( conflict )
+                res.status(409).json({error: violations.toString()});
+                console.log(violations.toString());
+            } else {
+                try {
+                    await connection.query('INSERT INTO "user" (email,name,contact) VALUES ($1,$2,$3)',
+                        [user.email, user.name, user.contact]);
+
+                    res.sendStatus(201);
+                } catch (e) {
+                    console.log(e);
+                    res.status(500).json({error: e.toString()}); /* Internal server error - can't save */
+                } finally {
+                    connection.release();
+                }
+            }
+
         }
     }
 }
