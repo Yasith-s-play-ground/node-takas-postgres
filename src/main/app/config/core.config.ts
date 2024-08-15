c188cc86a80e75c6d7a7f4a6c5fcfacc565663eb
Yasith Ishara Perera <yasithishara@gmail.com>
2024-08-15 17:39:06 +0530

    initializing controller if null

--- src/main/app/config/core.config.ts
+++ src/main/app/config/core.config.ts
@@ -1,5 +1,27 @@
 import express from "express";
 
+type Handler = {
+    path?: string,
+    method?: 'GET' | 'PUT' | 'POST' | 'PATCH' | 'DELETE',
+    middlewares?: Array<Function>
+}
+
+type Handlers = {
+    [handler: string]: Handler
+}
+
+type Controller = {
+    path?: string,
+    middlewares?: Array<Function>,
+    handlers?: Handlers
+}
+
+type Controllers = {
+    [controller: string]: Controller
+}
+
+const CONTROLLERS: Controllers = {};
+
 export function Module(controllers: Array<Function>) {
     return function (constructor: Function) {
     }
@@ -7,7 +29,7 @@ export function Module(controllers: Array<Function>) {
 
 export function RestController(path: string = "/") {
     return function (constructor: Function) {
-        console.log('Rest Controller');
+        CONTROLLERS[constructor.name].path = path;
     }
 }
 
@@ -18,31 +40,66 @@ export function Middleware(middlewares: Array<Function>) {
 }
 
 export function GetMapping(path: string = "/") {
-    return function (target: Object, name: string, descriptor: PropertyDescriptor) {
+    return function (prototype: Object, name: string, descriptor: PropertyDescriptor) {
+        if (!CONTROLLERS[prototype.constructor.name]) CONTROLLERS[prototype.constructor.name] = {
+            handlers: {}
+        };
+        CONTROLLERS[prototype.constructor.name].handlers![name] = {
+            path,
+            method: 'GET'
+        }
     }
 
 }
 
 export function PostMapping(path: string = "/") {
-    return function (target: Object, name: string, descriptor: PropertyDescriptor) {
+    return function (prototype: Object, name: string, descriptor: PropertyDescriptor) {
+        if (!CONTROLLERS[prototype.constructor.name]) CONTROLLERS[prototype.constructor.name] = {
+            handlers: {}
+        };
+        CONTROLLERS[prototype.constructor.name].handlers![name] = {
+            path,
+            method: 'POST'
+        }
     }
 
 }
 
 export function PutMapping(path: string = "/") {
-    return function (target: Object, name: string, descriptor: PropertyDescriptor) {
+    return function (prototype: Object, name: string, descriptor: PropertyDescriptor) {
+        if (!CONTROLLERS[prototype.constructor.name]) CONTROLLERS[prototype.constructor.name] = {
+            handlers: {}
+        };
+        CONTROLLERS[prototype.constructor.name].handlers![name] = {
+            path,
+            method: 'PUT'
+        }
     }
 
 }
 
 export function DeleteMapping(path: string = "/") {
-    return function (target: Object, name: string, descriptor: PropertyDescriptor) {
+    return function (prototype: Object, name: string, descriptor: PropertyDescriptor) {
+        if (!CONTROLLERS[prototype.constructor.name]) CONTROLLERS[prototype.constructor.name] = {
+            handlers: {}
+        };
+        CONTROLLERS[prototype.constructor.name].handlers![name] = {
+            path,
+            method: 'DELETE'
+        }
     }
 
 }
 
 export function PatchMapping(path: string = "/") {
-    return function (target: Object, name: string, descriptor: PropertyDescriptor) {
+    return function (prototype: Object, name: string, descriptor: PropertyDescriptor) {
+        if (!CONTROLLERS[prototype.constructor.name]) CONTROLLERS[prototype.constructor.name] = {
+            handlers: {}
+        };
+        CONTROLLERS[prototype.constructor.name].handlers![name] = {
+            path,
+            method: 'PATCH'
+        }
     }
 
 }
@@ -50,6 +107,7 @@ export function PatchMapping(path: string = "/") {
 export class ExpressApp {
     static create(module: Function) {
         const app = express();
+        console.log(CONTROLLERS);
         return app;
     }
 }
